66268f074ffb26f1bf16360881088b308c0bb330
moin <moinqureshi.ss@smartdatainc.io>
2023-04-10 16:01:41 +0530

    feat: setup plop for auto components/screen code generation

--- template/src/components/index.ts
+++ template/src/components/index.ts
@@ -1,4 +1,9 @@
 import AppContainer from './AppContainer/AppContainer'
 import AppLoader from './AppLoader/AppLoader'
+// PLOP COMPONENT IMPORTS
 
-export {AppContainer, AppLoader}
+export {
+  AppContainer,
+  AppLoader
+  // PLOP COMPONENT EXPORTS
+}
