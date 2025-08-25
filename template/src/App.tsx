bf3ba376e90b0cbe85f04036234ed89343a402ae
Kishan Daka <kishan_daka@bitnetinfotech.com>
2025-08-25 12:32:52 +0530

    fix: upgrade 0.80

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import {RootSiblingParent} from 'react-native-root-siblings'
+import {NavigationContainer} from '@react-navigation/native'
 
 import './i18n/i18n'
-import {AppAuthentication, AppProviders} from './components'
+import {AppLoader} from './components'
+import {AppStateContextProvider} from './hooks/useAppStateContext'
 import {AppNavigation} from './router'
+import {ThemeProvider} from './theme/ThemeProvider/ThemeProvider'
 
 const App = () => {
   return (
-    <AppProviders>
-      <AppAuthentication>
-        <AppNavigation />
-      </AppAuthentication>
-    </AppProviders>
+    <NavigationContainer>
+      <ThemeProvider >
+        <AppStateContextProvider >
+          <RootSiblingParent>
+            <AppNavigation />
+            <AppLoader />
+          </RootSiblingParent>
+        </AppStateContextProvider>
+      </ThemeProvider>
+    </NavigationContainer>
   )
 }
 
